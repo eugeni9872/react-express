605cec550b64f59cd830bc2603e40a43223a2312
Eugeni <ebejan@iesjoandaustria.org>
2018-11-01 14:25:03 +0100

    Style add

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import axios from 'axios';
+import { Card, Col, Row } from 'antd';
 
 
 class Posts extends Component {
@@ -12,15 +13,21 @@ class Posts extends Component {
 
     render(){
         return(
-            <div>
-               {this.props.posts.map((post, index) =>
-                (   
-                    <div key={index}>
-                        <h4>Title: {post.title}</h4>
-                        <p>{post.body}</p>
-                    </div>
-                ))}
-               
+            <div  style={{ background: '#ECECEC', padding: '30px'}}>   
+                <h2 style={{textAlign:'center'}}>Mostrando 100 posts </h2>
+                <Row  gutter={16}>
+                    {this.props.posts.map((post, index) =>
+                        (   
+                            <Col key={index} span={8} style={{marginTop:'20px', height:'200px' }}>
+                    
+                                <Card title={post.title} bordered={false}>
+                                <p>{post.body}</p>
+                                </Card>
+            
+                            </Col>
+                        ))}
+                    
+                    </Row>
             </div>
         )
     }
